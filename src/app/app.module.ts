7ea14745da205c9bf78a25bb1a9479ec4331ee90
unknown <natalieclark2021@gmail.com>
2022-06-02 20:01:36 -0500

    Created game-settings and game-play component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameSettingsComponent } from './game-settings/game-settings.component';
+import { GamePlayComponent } from './game-play/game-play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameSettingsComponent,
+    GamePlayComponent
   ],
   imports: [
     BrowserModule
